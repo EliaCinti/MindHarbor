070cb5f26d43b593f84f9efe8116e6683f0b7fd9
EliaCinti <cintielia21@gmail.com>
2025-02-02 12:42:57 +0100

    - Sistemata PatientDaoMySql

--- src/main/java/it/uniroma2/mindharbor/dao/mysql/PatientDaoMySql.java
+++ src/main/java/it/uniroma2/mindharbor/dao/mysql/PatientDaoMySql.java
@@ -1,12 +1,32 @@
 package it.uniroma2.mindharbor.dao.mysql;
 
+import it.uniroma2.mindharbor.beans.PatientBean;
 import it.uniroma2.mindharbor.beans.PsychologistBean;
+import it.uniroma2.mindharbor.beans.UserBean;
+import it.uniroma2.mindharbor.dao.PatientDao;
 import it.uniroma2.mindharbor.dao.PsychologistDao;
 import it.uniroma2.mindharbor.exception.DAOException;
+import it.uniroma2.mindharbor.model.Patient;
 
-public class PatientDaoMySql implements PsychologistDao {
+public class PatientDaoMySql implements PatientDao {
+
     @Override
-    public void savePsychologist(PsychologistBean psychologist) throws DAOException {
+    public void savePatient(PatientBean patient) throws DAOException {
+
+    }
+
+    @Override
+    public Patient retrievePatient(String username) throws DAOException {
+        return null;
+    }
+
+    @Override
+    public void updatePatient(Patient patient, UserBean user) throws DAOException {
+
+    }
+
+    @Override
+    public void deletePatient(String username) throws DAOException {
 
     }
 }
